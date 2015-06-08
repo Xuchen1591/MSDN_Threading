e9f9306985c2138d4199f7acfc44880b8ea9a75a
Xuchen1591 <magina.1591@outlook.com>
2015-06-09 07:31:31 +0800

    threading pool

--- MSDN_Threading/MSDN_Threading/Program.cs
+++ MSDN_Threading/MSDN_Threading/Program.cs
@@ -25,6 +25,87 @@ namespace MSDN_Threading
         }
     }
 
+    public class Producer
+    {
+        private Queue<int> _queue;
+
+        public Producer(Queue<int> q)
+        {
+            _queue = q;
+        }
+
+        public void ThreadRun()
+        {
+            int count = 0;
+            Random r = new Random();
+            lock((ICollection<int>)_queue)
+            {
+                while(_queue.Count < 20)
+                {
+                    _queue.Enqueue(r.Next(0,100));
+                    count++;
+                }
+            }
+            Console.WriteLine("producer thread produce {0} items", count);
+        }
+    }
+
+    public class Consumer
+    {
+        private Queue<int> _queue;
+
+        public Consumer(Queue<int> q)
+        {
+            _queue = q;
+        }
+
+        public void ThreadRun()
+        {
+            int count = 0;
+            lock((ICollection<int>)_queue)
+            {
+                int item = _queue.Dequeue();
+                count ++;
+            }
+            Console.WriteLine("consumer thread consume {0} items",count);
+        }
+    }
+
+    //lecture 3: threading pool
+    public class Fibonacci
+    {
+        private int _n;
+        public int N { get { return _n; } }
+
+        public int FibOfN { get { return _fibOfN; } }
+        private int _fibOfN;
+
+        private ManualResetEvent _doneEvent;
+        public Fibonacci(int n,ManualResetEvent doneEvent)
+        {
+            _n = n;
+            _doneEvent = doneEvent;
+        }
+
+        public void ThreadPoolCallback(Object threadContext)
+        {
+            int threadIndex = (int)threadContext;
+            Console.WriteLine("thread{0} started...", threadIndex);
+            _fibOfN = Calculate(_n);
+            Console.WriteLine("thread{0} calculated...", threadIndex);
+            _doneEvent.Set();
+        }
+
+        public int Calculate(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return Calculate(n - 1) + Calculate(n - 2);
+        }
+    }
+
     class Program
     {
         static AutoResetEvent autoEvent;
@@ -34,6 +115,17 @@ namespace MSDN_Threading
             Console.WriteLine("workers thread started, no waiting on event...");
             autoEvent.WaitOne();
             Console.WriteLine("reactived, now exiting...");
+        }
+
+        private static void ShowQueueContents(Queue<int> q)
+        {
+            lock((ICollection<int>)q)
+            {
+                foreach(int i in q)
+                    Console.Write(i + ",");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
 
         static void Main(string[] args)
@@ -54,6 +146,7 @@ namespace MSDN_Threading
              */
 
 
+            /*
             //lec2
             Worker worker = new Worker();
             Thread workerThread = new Thread(worker.doSomeThing);
@@ -66,6 +159,62 @@ namespace MSDN_Threading
             worker.requestStop();
             workerThread.Join();
             Console.WriteLine("main: work threading terminated...");
+             * */
+
+            /*
+            Queue<int> queue = new Queue<int>();
+
+            Console.WriteLine("configuring worker threads...");
+            Producer producer = new Producer(queue);
+            Consumer consumer = new Consumer(queue);
+            Thread producerThread = new Thread(producer.ThreadRun);
+            Thread consumerThread = new Thread(consumer.ThreadRun);
+
+            Console.WriteLine("launching producer and consumer threads...");
+            producerThread.Start();
+            consumerThread.Start();
+
+            for(int i=0;i<4;i++)
+            {
+                Thread.Sleep(2500);
+                ShowQueueContents(queue);
+            }
+
+            Console.WriteLine("signaling the threading to termibating...");
+
+            producerThread.Join();
+            consumerThread.Join();
+             * */
+
+
+            //lecture 3: threading pool
+            const int FibonacciCalculations = 10;
+
+            // One event is used for each Fibonacci object
+            ManualResetEvent[] doneEvents = new ManualResetEvent[FibonacciCalculations];
+            Fibonacci[] fibArray = new Fibonacci[FibonacciCalculations];
+            Random r = new Random();
+
+            // Configure and launch threads using ThreadPool:
+            Console.WriteLine("launching {0} tasks...", FibonacciCalculations);
+            for (int i = 0; i < FibonacciCalculations; i++)
+            {
+                doneEvents[i] = new ManualResetEvent(false);
+                Fibonacci f = new Fibonacci(r.Next(20, 40), doneEvents[i]);
+                fibArray[i] = f;
+                ThreadPool.QueueUserWorkItem(f.ThreadPoolCallback, i);
+            }
+
+            // Wait for all threads in pool to calculation...
+            WaitHandle.WaitAll(doneEvents);
+            Console.WriteLine("All calculations are complete.");
+
+            // Display the results...
+            for (int i = 0; i < FibonacciCalculations; i++)
+            {
+                Fibonacci f = fibArray[i];
+                Console.WriteLine("Fibonacci({0}) = {1}", f.N, f.FibOfN);
+            }
         }
     }
 }
